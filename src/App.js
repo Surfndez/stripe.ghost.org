05bda39f4494f3852eed1715926734ef12f95ddb
Fabien O'Carroll <fabien@allou.is>
2020-06-04 14:12:18 +0200

    Initial prototype of functionality
    no-issue

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import Switch, {Case} from './components/Switch';
+
+function LoadingState() {
+    return (
+        <span> Loading... </span>
+    );
+}
+
+/**
+ * @param {object} props
+ * @param {string} props.errorMessage
+ */
+function ErrorState(props) {
+    return (
+        <div>
+            <span> There was an error </span>
+            <span> {props.errorMessage} </span>
+        </div>
+    );
+}
+
+/**
+ * @param {object} props
+ * @param {string} props.token
+ */
+function SuccessState(props) {
+    return (
+        <div>
+            <span> Copy this stuff! </span>
+            <code> {props.token} </code>
+        </div>
+    );
+}
+
+async function getData() {
+    const res = await fetch(`/.netlify/functions/get-stripe-connect-integration-token${window.location.search}`, {
+        method: 'POST'
+    });
+
+    const text = await res.text();
+
+    if (!res.ok) {
+        throw new Error(text);
+    }
+
+    return text;
+}
 
 function App() {
+    const [state, setState] = useState({status: 'init', data: null});
+
+    const setSuccess = data => setState({status: 'success', data});
+    const setError = error => setState({status: 'error', data: error.message});
+
+    useEffect(() => {
+        setState({status: 'loading', data: null});
+        getData().then(setSuccess, setError);
+    }, []);
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-          Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-          Learn React
-                </a>
-            </header>
+            <main className="App-main">
+                <Switch on={state.status}>
+                    <Case when="loading">
+                        <LoadingState />
+                    </Case>
+                    <Case when="error">
+                        <ErrorState errorMessage={state.data} />
+                    </Case>
+                    <Case when="success">
+                        <SuccessState token={state.data} />
+                    </Case>
+                </Switch>
+            </main>
         </div>
     );
 }
