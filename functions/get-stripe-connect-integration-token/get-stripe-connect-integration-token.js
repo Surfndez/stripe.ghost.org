93c47f115f9537891c276a503dbe85ce8adac2c2
Fabien O'Carroll <fabien@allou.is>
2020-06-08 12:29:19 +0200

    Added ability to test function locally
    no-issue
    
    This uses a magicvalue TESTING as the state param, and makes testing the
    different type of errors easier

--- functions/get-stripe-connect-integration-token/get-stripe-connect-integration-token.js
+++ functions/get-stripe-connect-integration-token/get-stripe-connect-integration-token.js
@@ -51,6 +51,10 @@ async function handler(event) {
         };
     }
 
+    if (event.queryStringParameters.state === 'TESTING') {
+        return handleLocalTest(event.queryStringParameters.code);
+    }
+
     try {
         const stripe = getStripe();
         const tokenResponse = await stripe.oauth.token({
@@ -113,6 +117,42 @@ function encodeStripeTokenData(tokenResponse, s) {
     const {livemode: l, access_token: a, stripe_publishable_key: p} = tokenResponse;
 
     return Buffer.from(JSON.stringify({l, a, p, s})).toString('base64');
+}
+
+/**
+ * @param {string} code
+ *
+ * @returns {Response}
+ */
+function handleLocalTest(code) {
+    if (code === 'SUCCESS') {
+        return {
+            statusCode: 200,
+            body: ('FAKE_TOKEN').repeat(30)
+        };
+    }
+    if (code === 'EXPIRED') {
+        return {
+            statusCode: 500,
+            body: 'This authorization code has already been used. All tokens issued with this code have been revoked.'
+        };
+    }
+    if (code === 'INVALID') {
+        return {
+            statusCode: 500,
+            body: 'Authorization code does not exist: ac_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+        };
+    }
+    if (code === 'CONNECT') {
+        return {
+            statusCode: 500,
+            body: 'An error occurred with our connection to Stripe.'
+        };
+    }
+    return {
+        statusCode: 500,
+        body: 'An unknown error occured.'
+    };
 }
 
 module.exports = {
