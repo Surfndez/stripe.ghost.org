0970b9b5e2f67c7c694a484db4c12224623f7993
Fabien O'Carroll <fabien@allou.is>
2020-06-08 12:39:19 +0200

    Added a check for existence of query params
    no-issue
    
    We want to redirect to ghost.org if the page is visited without any
    query params

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import './index.css';
 import App from './App';
 import getData from './getData';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App getData={getData} />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+if (!window.location.search) {
+    window.location.href = 'https://ghost.org';
+} else {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App getData={getData} />
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
