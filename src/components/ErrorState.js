05c48d9aef3035b6e5f253ad50f52751b211c4e9
Aileen Nowak <AileenCGN@gmail.com>
2020-06-12 17:47:17 +1200

    Updated ErrorState to differentiate error screen depending on message

--- src/components/ErrorState.js
+++ src/components/ErrorState.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useState, useEffect} from 'react';
 import {ReactComponent as Warning} from '../assets/warning.svg';
 
 /**
@@ -7,14 +8,26 @@ import {ReactComponent as Warning} from '../assets/warning.svg';
  */
 function ErrorState(props) {
     const {errorMessage} = props;
-    console.log('ErrorState -> errorMessage', errorMessage);
+    const [state, setState] = useState({title: 'Default error title', message: 'An error occurred while connecting to Stripe. Please try again by clicking on “Connect with Stripe” in Ghost.'});
+
+    useEffect(() => {
+        if (errorMessage.match(/authorization code has already been used/igm)) {
+            setState({title: 'Code has been used already', message: 'An error occurred while connecting to Stripe. Please try again by clicking on “Connect with Stripe” in Ghost.'});
+        } else if (errorMessage.match(/authorization code does not exist/igm)) {
+            setState({title: 'Code does not exist', message: 'An error occurred while connecting to Stripe. Please try again by clicking on “Connect with Stripe” in Ghost.'});
+        } else if (errorMessage.match(/connection to Stripe/igm)) {
+            setState({title: 'Could not connect to Stripe', message: 'An error occurred while connecting to Stripe. Please try again by clicking on “Connect with Stripe” in Ghost.'});
+        } else if (errorMessage.match(/user denied your request/igm)) {
+            setState({title: 'You pressed cancel', message: 'An error occurred while connecting to Stripe. Please try again by clicking on “Connect with Stripe” in Ghost.'});
+        }
+    }, [errorMessage]);
+
     return (
         <div className="gh-box gh-error-box">
             <Warning />
             <div className="gh-error-content">
-                <h2>Could not connect to Stripe</h2>
-                <p>An error occurred while connecting to Stripe. Please try again by clicking on “Connect with Stripe” in Ghost.</p>
-                <code>{errorMessage}</code>
+                <h2>{state.title}</h2>
+                <p>{state.message}</p>
             </div>
         </div>
     );
